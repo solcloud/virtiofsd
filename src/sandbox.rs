3ba90ca6600ede909b7b8ccbe3e2a1271706998c
Sergio Lopez <slp@redhat.com>
2021-04-05 17:39:38 +0200

    Set up uid/gid mappings in user_namespace sandbox
    When using the namespace sandbox mode with a non-privileged user, set
    up 1-to-1 mappings for the current process uid and gid. This allows
    both root and a user with the same uid/gid in the VM to access the
    files served by us. This scenario fits nicely with the "shared folder"
    use case.
    
    Signed-off-by: Sergio Lopez <slp@redhat.com>

--- src/sandbox.rs
+++ src/sandbox.rs
@@ -70,6 +70,12 @@ pub enum Error {
     InvalidNrOpen(std::num::ParseIntError),
     /// Failed to set rlimit.
     SetRlimit(io::Error),
+    /// Failed to write to `/proc/self/gid_map`.
+    WriteGidMap(io::Error),
+    /// Failed to write to `/proc/self/setgroups`.
+    WriteSetGroups(io::Error),
+    /// Failed to write to `/proc/self/uid_map`.
+    WriteUidMap(io::Error),
     /// Sandbox mode unavailable for non-privileged users
     SandboxModeInvalidUID,
 }
@@ -376,8 +382,23 @@ impl Sandbox {
         }
     }
 
+    /// Sets 1-to-1 mappings for the current uid and gid.
+    fn setup_id_mappings(&self, uid: u32, gid: u32) -> Result<(), Error> {
+        // To be able to set up the gid mapping, we're required to disable setgroups(2) first.
+        fs::write("/proc/self/setgroups", "deny\n").map_err(Error::WriteSetGroups)?;
+
+        // Set up 1-to-1 mappings for our uid and gid.
+        let uid_mapping = format!("{} {} 1\n", uid, uid);
+        fs::write("/proc/self/uid_map", uid_mapping).map_err(Error::WriteUidMap)?;
+
+        let gid_mapping = format!("{} {} 1\n", gid, gid);
+        fs::write("/proc/self/gid_map", gid_mapping).map_err(Error::WriteGidMap)?;
+        Ok(())
+    }
+
     pub fn enter_namespace(&mut self) -> Result<Option<i32>, Error> {
         let uid = unsafe { libc::geteuid() };
+        let gid = unsafe { libc::getegid() };
 
         let flags = if uid == 0 {
             libc::CLONE_NEWPID | libc::CLONE_NEWNS | libc::CLONE_NEWNET
@@ -401,6 +422,8 @@ impl Sandbox {
                 unsafe { libc::prctl(libc::PR_SET_PDEATHSIG, libc::SIGTERM) };
                 if uid == 0 {
                     self.setup_nofile_rlimit()?;
+                } else {
+                    self.setup_id_mappings(uid, gid)?;
                 }
                 self.setup_mounts()?;
                 Ok(None)
