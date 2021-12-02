solcloud/virtiofsd

commit 16ffcc6ba73e4be5ceb202b76f2eac8bf683f3bf

Open /proc/self/mountinfo
Generating a file handle yields a mount ID for which it is valid.
Opening the handle however requires specifying an FD on that mount.  To
get this FD, right now we just open the first file that is looked up on
said mount; but we really want to use the mount point instead[1],
because with inotify support coming at some point, we want the inode
belonging to the mount FD to be deletable (such that the guest receives
a notification about the deletion).  There are two reasons why that
means we want to use mount points to get mount FDs:

(1) The mount point can only be deleted once everything else in the
    mount has been deleted, too.  That means all of our references to
    such inodes must be gone, and all file handles requiring the
    respective mount FD are gone, too, so we can close the mount FD at
    that point.
    (To do that, we need mount FD refcounting, which is added by a later
    patch.)

(2) (Host) mount points cannot be deleted anyway.  Attempting to do so
    will return EBUSY.  Using them for mount FDs means that when it
    comes to deletion notifications they do not play a role.

This commit makes virtiofsd-rs open /proc/self/mountinfo, first, if
necessary, in the sandboxing code, and then in `PassthroughFs::new()`
otherwise.  This is just how it works for /proc/self/fd.

We do not make use of mountinfo's contents yet, though; that is left for
the next commit.  Until then, `PassthroughFs::new()`'s `mountinfo_fd`
has to remain unused.

[1] In practice, both are probably going to be the same (the mount point
is probably the first inode of a submount that we look up).  However,
making this explicit is better.

Signed-off-by: Hanna Reitz <hreitz@redhat.com>

src/passthrough/mod.rs
@@ -360,6 +360,13 @@ pub struct Config {
     /// The default is `None`.
     pub proc_sfd_rawfd: Option<File>,
 
+    /// Optional `File` object for /proc/self/mountinfo.  Callers can open a `File` and pass it
+    /// here, so there is no need to open it in PassthroughFs::new().  This is especially useful
+    /// for sandboxing.
+    ///
+    /// The default is `None`.
+    pub proc_mountinfo_rawfd: Option<File>,
+
     /// Whether the file system should announce submounts to the guest.  Not doing so means that
     /// the FUSE client may see st_ino collisions: This stat field is passed through, so if the
     /// shared directory encompasses multiple mounts, some inodes (in different file systems) may
@@ -411,6 +418,7 @@ impl Default for Config {
             xattrmap: None,
             xattr_security_capability: None,
             proc_sfd_rawfd: None,
+            proc_mountinfo_rawfd: None,
             announce_submounts: false,
             inode_file_handles: false,
             readdirplus: true,
@@ -480,6 +488,17 @@ impl PassthroughFs {
             "/",
             libc::O_PATH | libc::O_NOFOLLOW | libc::O_CLOEXEC,
         )?;
+
+        #[allow(unused_variables)] // TODO: Remove once used
+        let mountinfo_fd = if let Some(fd) = cfg.proc_mountinfo_rawfd.take() {
+            fd
+        } else {
+            openat(
+                &libc::AT_FDCWD,
+                "/proc/self/mountinfo",
+                libc::O_PATH | libc::O_NOFOLLOW | libc::O_CLOEXEC,
+            )?
+        };
 
         // Check for statx() system call
         let use_statx = match statx(&proc_self_fd, None) {

src/main.rs
@@ -721,6 +721,7 @@ fn main() {
             xattr,
             xattrmap,
             proc_sfd_rawfd: sandbox.get_proc_self_fd(),
+            proc_mountinfo_rawfd: sandbox.get_mountinfo_fd(),
             announce_submounts: opt.announce_submounts,
             inode_file_handles: opt.inode_file_handles,
             readdirplus,

src/sandbox.rs
@@ -6,7 +6,7 @@ use std::ffi::CString;
 use std::fmt;
 use std::fs::{self, File};
 use std::io;
-use std::os::unix::io::FromRawFd;
+use std::os::unix::io::{AsRawFd, FromRawFd};
 use std::str::FromStr;
 
 use tempdir::TempDir;
@@ -41,10 +41,14 @@ pub enum Error {
     MountNewRoot(io::Error),
     /// Error mounting target directory.
     MountTarget(io::Error),
+    /// Failed to open `/proc/self/mountinfo`.
+    OpenMountinfo(io::Error),
     /// Failed to open new root.
     OpenNewRoot(io::Error),
     /// Failed to open old root.
     OpenOldRoot(io::Error),
+    /// Failed to open `/proc/self`.
+    OpenProcSelf(io::Error),
     /// Failed to open `/proc/self/fd`.
     OpenProcSelfFd(io::Error),
     /// Error switching root directory.
@@ -101,6 +105,8 @@ pub struct Sandbox {
     shared_dir: String,
     /// A `File` object for `/proc/self/fd` obtained from the sandboxed context.
     proc_self_fd: Option<File>,
+    /// A `File` object for `/proc/self/mountinfo` obtained from the sandboxed context.
+    mountinfo_fd: Option<File>,
     /// Mechanism to be used for setting up the sandbox.
     sandbox_mode: SandboxMode,
     /// Value to set as RLIMIT_NOFILE
@@ -112,13 +118,14 @@ impl Sandbox {
         Sandbox {
             shared_dir,
             proc_self_fd: None,
+            mountinfo_fd: None,
             sandbox_mode,
             rlimit_nofile,
         }
     }
 
-    // Make `self.shared_dir` our root directory, and get an isolated file descriptor for
-    // `/proc/self/fd`.
+    // Make `self.shared_dir` our root directory, and get isolated file descriptors for
+    // `/proc/self/fd` and '/proc/self/mountinfo`.
     //
     // This is based on virtiofsd's setup_namespaces() and setup_mounts(), and it's very similar to
     // the strategy used in containers. Consists on a careful sequence of mounts and bind-mounts to
@@ -127,6 +134,19 @@ impl Sandbox {
     //
     // It's ugly, but it's the only way until Linux implements a proper containerization API.
     fn setup_mounts(&mut self) -> Result<(), Error> {
+        // Open an FD to `/proc/self` so we can later open `/proc/self/mountinfo`.
+        // (If we opened `/proc/self/mountinfo` now, it would appear empty by the end of this
+        // function, which is why we need to defer opening it until then.)
+        let c_proc_self = CString::new("/proc/self").unwrap();
+        let proc_self_raw = unsafe { libc::open(c_proc_self.as_ptr(), libc::O_PATH) };
+        if proc_self_raw < 0 {
+            return Err(Error::OpenProcSelf(std::io::Error::last_os_error()));
+        }
+
+        // Encapsulate the `/proc/self` FD in a `File` object so it is closed when this function
+        // returns
+        let proc_self = unsafe { File::from_raw_fd(proc_self_raw) };
+
         // Ensure our mount changes don't affect the parent mount namespace.
         let c_root_dir = CString::new("/").unwrap();
         let ret = unsafe {
@@ -289,6 +309,16 @@ impl Sandbox {
         unsafe { libc::close(newroot_fd) };
         unsafe { libc::close(oldroot_fd) };
 
+        // Open `/proc/self/mountinfo` now
+        let c_mountinfo = CString::new("mountinfo").unwrap();
+        let mountinfo_fd =
+            unsafe { libc::openat(proc_self.as_raw_fd(), c_mountinfo.as_ptr(), libc::O_RDONLY) };
+        if mountinfo_fd < 0 {
+            return Err(Error::OpenMountinfo(std::io::Error::last_os_error()));
+        }
+        // Safe because we just opened this fd.
+        self.mountinfo_fd = Some(unsafe { File::from_raw_fd(mountinfo_fd) });
+
         Ok(())
     }
 
@@ -361,6 +391,14 @@ impl Sandbox {
         // Safe because we just opened this fd.
         self.proc_self_fd = Some(unsafe { File::from_raw_fd(proc_self_fd) });
 
+        let c_mountinfo = CString::new("/proc/self/mountinfo").unwrap();
+        let mountinfo_fd = unsafe { libc::open(c_mountinfo.as_ptr(), libc::O_RDONLY) };
+        if mountinfo_fd < 0 {
+            return Err(Error::OpenMountinfo(std::io::Error::last_os_error()));
+        }
+        // Safe because we just opened this fd.
+        self.mountinfo_fd = Some(unsafe { File::from_raw_fd(mountinfo_fd) });
+
         let c_shared_dir = CString::new(self.shared_dir.clone()).unwrap();
         let ret = unsafe { libc::chroot(c_shared_dir.as_ptr()) };
         if ret != 0 {
@@ -390,6 +428,10 @@ impl Sandbox {
 
     pub fn get_proc_self_fd(&mut self) -> Option<File> {
         self.proc_self_fd.take()
+    }
+
+    pub fn get_mountinfo_fd(&mut self) -> Option<File> {
+        self.mountinfo_fd.take()
     }
 
     pub fn get_root_dir(&self) -> String {

commit b9ac8c31682f42fded0a7726af5b10a170d16e88

Correctly open mountinfo with sandbox=none
We need to open /proc/self/mountinfo with O_RDONLY, because we want to
read from it.  Instead, we open it with O_PATH, though, which is wrong
and will later prevent us from creating openable file handles.

Fix it.

Reported-by: German Maglione <gmaglione@redhat.com>
Fixes: 16ffcc6ba73e4be5ceb202b76f2eac8bf683f3bf
       ("Open /proc/self/mountinfo")
Signed-off-by: Hanna Reitz <hreitz@redhat.com>

src/passthrough/mod.rs
@@ -551,7 +551,7 @@ impl PassthroughFs {
             openat(
                 &libc::AT_FDCWD,
                 "/proc/self/mountinfo",
-                libc::O_PATH | libc::O_NOFOLLOW | libc::O_CLOEXEC,
+                libc::O_RDONLY | libc::O_NOFOLLOW | libc::O_CLOEXEC,
             )?
         };
 