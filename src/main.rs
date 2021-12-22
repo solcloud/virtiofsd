solcloud/virtiofsd

commit e662b6721eab1d77c31bed8b600bc9e2994112a9

file_handle: Use mount points for mount FDs
As explained in HEAD^, we want to use mount points for the mount FDs we
open.  This commit puts a `File` for /proc/self/mountinfo into
`MountFds`, adds a function to parse its content to find the mount point
for a given mount ID (`MountFds.get_mount_root()`), and lets
`FileHandle.from_name_at_with_mount_fds()` use it to open this mount
point as the mount FD for a given mount ID.

Signed-off-by: Hanna Reitz <hreitz@redhat.com>

src/passthrough/file_handle.rs
@@ -4,11 +4,11 @@
 
 use crate::passthrough::stat::statx;
 use std::collections::HashMap;
-use std::ffi::CStr;
+use std::ffi::{CStr, CString};
 use std::fs::File;
-use std::io;
+use std::io::{self, Read, Seek, SeekFrom};
 use std::os::unix::io::{AsRawFd, FromRawFd, RawFd};
-use std::sync::RwLock;
+use std::sync::{Mutex, RwLock};
 
 const MAX_HANDLE_SZ: usize = 128;
 
@@ -17,9 +17,11 @@ const MAX_HANDLE_SZ: usize = 128;
  * respective mount.  This is a type in which we can store fds that we know are associated with a
  * given mount ID, so that when opening a handle we can look it up.
  */
-#[derive(Default)]
 pub struct MountFds {
     map: RwLock<HashMap<u64, File>>,
+
+    /// /proc/self/mountinfo
+    mountinfo: Mutex<File>,
 }
 
 #[derive(Clone, Copy, PartialOrd, Ord, PartialEq, Eq)]
@@ -55,8 +57,42 @@ extern "C" {
 }
 
 impl MountFds {
-    pub fn new() -> Self {
-        MountFds::default()
+    pub fn new(mountinfo: File) -> Self {
+        MountFds {
+            map: Default::default(),
+            mountinfo: Mutex::new(mountinfo),
+        }
+    }
+
+    /// Given a mount ID, return the mount root path (by reading `/proc/self/mountinfo`)
+    fn get_mount_root(&self, mount_id: u64) -> io::Result<String> {
+        let mountinfo = {
+            let mountinfo_file = &mut *self.mountinfo.lock().unwrap();
+
+            mountinfo_file.seek(SeekFrom::Start(0))?;
+
+            let mut mountinfo = String::new();
+            mountinfo_file.read_to_string(&mut mountinfo)?;
+
+            mountinfo
+        };
+
+        let path = mountinfo.split('\n').find_map(|line| {
+            let mut columns = line.split(char::is_whitespace);
+
+            if columns.next()?.parse::<u64>().ok()? != mount_id {
+                return None;
+            }
+
+            // Skip parent mount ID, major:minor device ID, and the root within the filesystem
+            // (to get to the mount path)
+            columns.nth(3)
+        });
+
+        match path {
+            Some(p) => Ok(String::from(p)),
+            None => Err(io::Error::from_raw_os_error(libc::EINVAL)),
+        }
     }
 }
 
@@ -108,37 +144,40 @@ impl FileHandle {
         let handle = Self::from_name_at(dir, path)?;
 
         if !mount_fds.map.read().unwrap().contains_key(&handle.mnt_id) {
-            // `open_by_handle_at()` needs a non-`O_PATH` fd, which we will need to open here.
-            // We do not know whether `dir`/`path` is a special file, though, and we must not open
-            // special files with anything but `O_PATH`, so we have to get some `O_PATH` fd first
-            // that we can stat to find out whether it is safe to open.
-            // (When opening a new fd here, keep a `File` object around so the fd is closed when it
-            // goes out of scope.)
-            let (path_fd, _path_file) = if path.to_bytes().is_empty() {
-                (dir.as_raw_fd(), None)
-            } else {
-                let ret = unsafe { libc::openat(dir.as_raw_fd(), path.as_ptr(), libc::O_PATH) };
-                if ret < 0 {
-                    return Err(io::Error::last_os_error());
-                }
-                // `openat()` guarantees `ret` is a valid fd
-                (ret, Some(unsafe { File::from_raw_fd(ret) }))
-            };
+            // `open_by_handle_at()` needs a non-`O_PATH` fd, which we will need to open here.  We
+            // are going to open the filesystem's mount point, but we do not know whether that is a
+            // special file[1], and we must not open special files with anything but `O_PATH`, so
+            // we have to get some `O_PATH` fd first that we can stat to find out whether it is
+            // safe to open.
+            // [1] While mount points are commonly directories, it is entirely possible for a
+            //     filesystem's root inode to be a regular or even special file.
+            let mount_point = mount_fds.get_mount_root(handle.mnt_id)?;
+            let c_mount_point = CString::new(mount_point)?;
+            let mount_point_fd = unsafe { libc::open(c_mount_point.as_ptr(), libc::O_PATH) };
+            if mount_point_fd < 0 {
+                return Err(io::Error::last_os_error());
+            }
 
-            // Ensure that `path_fd` refers to an inode with the mount ID we need
-            let stx = statx(&path_fd, None)?;
+            // Safe because we have just opened this FD
+            let mount_point_path = unsafe { File::from_raw_fd(mount_point_fd) };
+
+            // Ensure that `mount_point_path` refers to an inode with the mount ID we need
+            let stx = statx(&mount_point_path, None)?;
             if stx.mnt_id != handle.mnt_id {
                 return Err(io::Error::from_raw_os_error(libc::EIO));
             }
 
-            // Ensure that we can safely reopen `path_fd` with `O_RDONLY`
+            // Ensure that we can safely reopen `mount_point_path` with `O_RDONLY`
             let file_type = stx.st.st_mode & libc::S_IFMT;
             if file_type != libc::S_IFREG && file_type != libc::S_IFDIR {
                 return Err(io::Error::from_raw_os_error(libc::EIO));
             }
 
             // Now that we know that this is a regular file or directory, really open it
-            let file = reopen_fd(path_fd, libc::O_RDONLY | libc::O_NOFOLLOW | libc::O_CLOEXEC)?;
+            let file = reopen_fd(
+                mount_point_path.as_raw_fd(),
+                libc::O_RDONLY | libc::O_NOFOLLOW | libc::O_CLOEXEC,
+            )?;
 
             mount_fds.map.write().unwrap().insert(handle.mnt_id, file);
         }

src/passthrough/mod.rs
@@ -489,7 +489,6 @@ impl PassthroughFs {
             libc::O_PATH | libc::O_NOFOLLOW | libc::O_CLOEXEC,
         )?;
 
-        #[allow(unused_variables)] // TODO: Remove once used
         let mountinfo_fd = if let Some(fd) = cfg.proc_mountinfo_rawfd.take() {
             fd
         } else {
@@ -511,7 +510,7 @@ impl PassthroughFs {
             next_inode: AtomicU64::new(fuse::ROOT_ID + 1),
             handles: RwLock::new(BTreeMap::new()),
             next_handle: AtomicU64::new(0),
-            mount_fds: MountFds::new(),
+            mount_fds: MountFds::new(mountinfo_fd),
             proc_self_fd,
             root_fd,
             writeback: AtomicBool::new(false),

commit 8a5ad1d1722dd993fbcb31a35275a9be1431e01e

file_handle: Strip prefix from mount points
/proc/self/mountinfo's content is not affected by chroot(), so in
sandbox=chroot mode, we need to remove the shared directory path as a
prefix from the mount points it lists.

Reported-by: German Maglione <gmaglione@redhat.com>
Fixes: e662b6721eab1d77c31bed8b600bc9e2994112a9
       ("file_handle: Use mount points for mount FDs")
Signed-off-by: Hanna Reitz <hreitz@redhat.com>

src/main.rs
@@ -776,6 +776,7 @@ fn main() {
         None => passthrough::Config {
             cache_policy: opt.cache,
             root_dir: sandbox.get_root_dir(),
+            mountinfo_prefix: sandbox.get_mountinfo_prefix(),
             xattr,
             xattrmap,
             proc_sfd_rawfd: sandbox.get_proc_self_fd(),

src/passthrough/file_handle.rs
@@ -29,6 +29,9 @@ pub struct MountFds {
 
     /// /proc/self/mountinfo
     mountinfo: Mutex<File>,
+
+    /// An optional prefix to strip from all mount points in mountinfo
+    mountprefix: Option<String>,
 }
 
 #[derive(Clone, Copy, PartialOrd, Ord, PartialEq, Eq)]
@@ -69,10 +72,11 @@ extern "C" {
 }
 
 impl MountFds {
-    pub fn new(mountinfo: File) -> Self {
+    pub fn new(mountinfo: File, mountprefix: Option<String>) -> Self {
         MountFds {
             map: Default::default(),
             mountinfo: Mutex::new(mountinfo),
+            mountprefix,
         }
     }
 
@@ -102,7 +106,21 @@ impl MountFds {
         });
 
         match path {
-            Some(p) => Ok(String::from(p)),
+            Some(p) => {
+                let p = String::from(p);
+                if let Some(prefix) = self.mountprefix.as_ref() {
+                    if let Some(suffix) = p.strip_prefix(prefix) {
+                        Ok(suffix.into())
+                    } else {
+                        // Mount is outside the shared directory, so it must be the mount the root
+                        // directory is on
+                        Ok("/".into())
+                    }
+                } else {
+                    Ok(p)
+                }
+            }
+
             None => Err(io::Error::from_raw_os_error(libc::EINVAL)),
         }
     }

src/passthrough/mod.rs
@@ -425,6 +425,11 @@ pub struct Config {
     /// The default is `/`.
     pub root_dir: String,
 
+    /// A prefix to strip from the mount points listed in /proc/self/mountinfo.
+    ///
+    /// The default is `None`.
+    pub mountinfo_prefix: Option<String>,
+
     /// Whether the file system should support Extended Attributes (xattr). Enabling this feature may
     /// have a significant impact on performance, especially on write parallelism. This is the result
     /// of FUSE attempting to remove the special file privileges after each write request.
@@ -502,6 +507,7 @@ impl Default for Config {
             cache_policy: Default::default(),
             writeback: false,
             root_dir: String::from("/"),
+            mountinfo_prefix: None,
             xattr: false,
             xattrmap: None,
             xattr_security_capability: None,
@@ -598,7 +604,7 @@ impl PassthroughFs {
             next_inode: AtomicU64::new(fuse::ROOT_ID + 1),
             handles: RwLock::new(BTreeMap::new()),
             next_handle: AtomicU64::new(0),
-            mount_fds: MountFds::new(mountinfo_fd),
+            mount_fds: MountFds::new(mountinfo_fd, cfg.mountinfo_prefix.clone()),
             proc_self_fd,
             root_fd,
             writeback: AtomicBool::new(false),

src/sandbox.rs
@@ -465,4 +465,13 @@ impl Sandbox {
             SandboxMode::None => self.shared_dir.clone(),
         }
     }
+
+    /// Return the prefix to strip from /proc/self/mountinfo entries to get paths that are actually
+    /// accessible in our sandbox
+    pub fn get_mountinfo_prefix(&self) -> Option<String> {
+        match self.sandbox_mode {
+            SandboxMode::Namespace | SandboxMode::None => None,
+            SandboxMode::Chroot => Some(self.shared_dir.clone()),
+        }
+    }
 }