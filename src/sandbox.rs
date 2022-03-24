66e2cb0e550874cca6e56c893ce0ce603f430488
Vivek Goyal <vgoyal@redhat.com>
2022-03-24 18:27:17 -0400

    Do not drop supplemental groups for unprivileged user namespace
    Right now we call setgroups() and drop all supplementary groups for all
    the sandboxing modes. This does not work for the case of "--sandbox=Namespace"
    when launched by unprivileged user as we don't have permission to call
    setgroups()  (neither outside namespace nor inside namespace).
    
    When running unprivileged and not using newuidmap/newgidmap, we have ownership
    of only unprivileged id and can map only that user. Previous patch added
    the code to create 1:1 mapping of uid/gid. That means there is no arbitray
    switching. All the files owned by user show up as same uid inside guest.
    Andything not owned by uid/gid showsup as "nobody".
    
    Given there is no arbitrary switching of uid/gid and everything in the
    fs is owned by one uid/gid (or nobody), I think a particular
    issue which required clearing setgroups() does not arise in this
    configuration. So it might not be a necessacity to drop membership
    of all supplementary groups.
    
    So with "--sandbox=Namespace" launched by unprivileged user, do not
    drop supplementary groups (setgroups()). This is get this mode working
    again. 
    
    If this becomes a problem ever again, then we will have to look at
    using newuidmap/newgidmap so that we have capability to call setgroups()
    inside user namespace and then we can call setgroups() and drop all
    the supplementary groups. 
    
    Signed-off-by: Vivek Goyal <vgoyal@redhat.com>

--- src/sandbox.rs
+++ src/sandbox.rs
@@ -409,6 +409,21 @@ impl Sandbox {
             libc::CLONE_NEWPID | libc::CLONE_NEWNS | libc::CLONE_NEWNET | libc::CLONE_NEWUSER
         };
 
+        // Drop supplemental groups. This is running as root and will
+        // support arbitrary uid/gid switching and we don't want to
+        // retain membership of any supplementary groups.
+        //
+        // This is not necessarily required for non-root case, where
+        // unprivileged user has started us, we will setup one user
+        // namespace with 1:1 mapping and there is no arbitrary uid/gid
+        // switching at all. In this mode setgroups() is not allowed, so
+        // we can't drop supplementary groups even if wanted to. Only
+        // way to do this will be to use newuidmap/newgidmap to setup
+        // user namespace which will allow setgroups().
+        if uid == 0 {
+            self.drop_supplemental_groups()?;
+        }
+
         let ret = unsafe { libc::unshare(flags) };
         if ret != 0 {
             return Err(Error::Unshare(std::io::Error::last_os_error()));
@@ -498,8 +513,9 @@ impl Sandbox {
         }
 
         // Unconditionally drop supplemental groups for every sandbox mode.
-        self.drop_supplemental_groups()?;
-
+        if self.sandbox_mode != SandboxMode::Namespace {
+            self.drop_supplemental_groups()?;
+        }
         match self.sandbox_mode {
             SandboxMode::Namespace => self.enter_namespace(),
             SandboxMode::Chroot => self.enter_chroot(),
